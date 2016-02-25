2de60da031ea9a555bc33e2796ea990b94887c49
Kenneth Foner <kenny.foner@gmail.com>
2016-02-24 23:44:55 -0500

    Added examples file

---
+++ Examples.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Examples where
+
+import SimpleSolver
+
+-- EXAMPLE INPUTS
+
+-- This loops the solver if addToWorkList is wrong
+-- Note: Prepend New Work to the Work List
+ab_bc_ca :: [Equality]
+ab_bc_ca = [constraints| x ~ y, y ~ z, z ~ x |]
+
+-- This loops the solver if we process FunCans first
+-- Note: Ordered Processing of Flattening Results
+f_int_bool :: [Equality]
+f_int_bool = [constraints| F(Int) ~ Bool |]
+
+
